9cf6b1aff0073da46da3ea74d9b1b33cdb9603fb
r2r <aart.st@gmail.com>
2021-01-13 14:05:45 +0100

    add support for basic shopping list creation

---
+++ shopping_list/domain/models.py
@@ -0,0 +1,47 @@
+import enum
+from typing import List, Dict
+
+import attr
+from pydantic import BaseModel
+
+
+class QuantityUnits(enum.IntEnum):
+    units = enum.auto()
+    gram = enum.auto()
+
+
+class Ingredient(BaseModel):
+    name: str
+    quantity: float
+    unit: QuantityUnits
+
+
+class IngredientInFridge(Ingredient):
+    allocated_quantity: float = 0.0
+
+
+class Recipe(BaseModel):
+    ingredients: List[Ingredient]
+
+
+@attr.s
+class ShoppingList:
+    _ingredients_in_fridge: List[IngredientInFridge] = attr.ib()
+    items: Dict[str, float] = attr.ib(factory=dict)
+
+    def create(self, recipes: List[Recipe]):
+        for recipe in recipes:
+            for ingredient_in_recipe in recipe.ingredients:
+                try:
+                    ingredient_in_fridge = next(filter(lambda in_fridge: in_fridge.name == ingredient_in_recipe.name,
+                                                       self._ingredients_in_fridge))
+                    quantity_to_buy = ingredient_in_recipe.quantity - ingredient_in_fridge.quantity
+                    # neglect possible mismatch of units for now
+                    if ingredient_in_fridge.quantity < ingredient_in_recipe.quantity:
+                        if ingredient_in_recipe.name not in self.items:
+                            self.items[ingredient_in_recipe.name] = quantity_to_buy
+                        else:
+                            self.items[ingredient_in_recipe.name] += quantity_to_buy
+                    ingredient_in_fridge.allocated_quantity = ingredient_in_fridge.quantity if quantity_to_buy >= 0 else ingredient_in_recipe.quantity
+                except StopIteration:
+                    self.items[ingredient_in_recipe.name] = ingredient_in_recipe.quantity
---
+++ tests/domain/test_shopping_list.py
@@ -0,0 +1,21 @@
+from shopping_list.domain import models
+
+
+def test_create_shopping_list_for_single_recipe__some_ingredients_missing():
+    ingredients_in_fridge = [models.IngredientInFridge(name='banana', quantity=4, unit=models.QuantityUnits.units),
+                             models.IngredientInFridge(name='avocado', quantity=1, unit=models.QuantityUnits.units),
+                             models.IngredientInFridge(name='salad', quantity=1, unit=models.QuantityUnits.units)]
+
+    recipe = models.Recipe(ingredients=[models.Ingredient(name='salad', quantity=1, unit=models.QuantityUnits.units),
+                                        models.Ingredient(name='pear', quantity=1, unit=models.QuantityUnits.units),
+                                        models.Ingredient(name='almond', quantity=40, unit=models.QuantityUnits.gram),
+                                        models.Ingredient(name='goat cheese', quantity=80,
+                                                          unit=models.QuantityUnits.gram)])
+
+    shopping_list = models.ShoppingList(ingredients_in_fridge=ingredients_in_fridge)
+    shopping_list.create([recipe])
+
+    assert shopping_list.items == {'pear': 1,
+                                   'almond': 40,
+                                   'goat cheese': 80}
+    assert ingredients_in_fridge[2].allocated_quantity == 1.0
