6a5b44db770f6a06ec149264deb50b4eb7e52437
r2r <aart.st@gmail.com>
2021-01-15 12:21:44 +0100

    modify conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,10 @@ import pytest
 from sqlalchemy.orm import Session
 
 from shopping_list.adapters import db as _db
+from shopping_list.bootstrap import bootstrap
 
 
 @pytest.fixture
 def db() -> Session:
-    _db.model_base.Base.metadata.create_all(bind=_db.session.engine)
+    bootstrap()
     yield _db.session.Session()
